8a5ae98e18ce883b7d23ef480c34b3490fe65416
LCJ-MinYa <1049468118@qq.com>
2018-04-03 18:23:53 +0800

    搜索建议

--- src/controller/life/search/searchSuggestion.js
+++ src/controller/life/search/searchSuggestion.js
@@ -6,10 +6,19 @@ export default class extends BaseRest {
         if (this.isGet) {
             const HttpRequest = think.service('httpRequest');
             const param = this.get();
+            if (!param.wd) {
+                param.wd = '';
+            }
             let result = await HttpRequest.GetService('http://suggestion.baidu.com/su?ie=UTF-8&wd=' + param.wd);
             let jsonStr = Utils.getBetweenTwoStringContent(result.body, '[', ']', true);
             try {
-                this.success(JSON.parse(jsonStr), '获取搜索建议文本成功');
+                let resultArr = JSON.parse(jsonStr);
+                for (let i = 0; i < resultArr.length; i++) {
+                    resultArr[i] = {
+                        value: resultArr[i]
+                    }
+                }
+                this.success(resultArr, '获取搜索建议文本成功');
             } catch (err) {
                 this.fail(500, '解析搜索结果失败');
             }
