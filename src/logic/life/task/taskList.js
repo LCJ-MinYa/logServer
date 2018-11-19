92329446c558eca413ac676f307e7fd8612e9725
MinYa <1049468118@qq.com>
2018-11-19 17:45:50 +0800

    任务管理

---
+++ src/logic/life/task/taskList.js
@@ -0,0 +1,12 @@
+export default class TaskList extends think.Logic {
+    indexAction() {
+        if (this.isPost) {
+            const param = this.post();
+
+            if (!param.uid) {
+                this.fail(400, '未获取到用户信息，请重新登陆!');
+                return false;
+            }
+        }
+    }
+}
