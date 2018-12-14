d919231ebfa524e72ef2e0cf614432e8d019d792
MinYa <1049468118@qq.com>
2018-12-14 16:10:08 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#!/bin/node
 
 cd /root/www/logServer
 git pull
-npm run build
+apidoc -i src/ -o view/index
+node babel --no-babelrc src/ --presets think-node --out-dir app/
 pm2 restart logServer
