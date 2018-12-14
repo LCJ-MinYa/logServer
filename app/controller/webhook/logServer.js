8e5e53de79ba5bef326b6c905e2e3773558d2eee
MinYa <1049468118@qq.com>
2018-12-14 18:43:56 +0800

    更新api文档

--- app/controller/webhook/logServer.js
+++ app/controller/webhook/logServer.js
@@ -20,22 +20,11 @@ const exec = require('child_process').exec;
  * @apiGroup webhookGroup
  * @apiVersion 1.0.0
  *
- * @apiParam {String} type 密码类型
- *
- * @apiParamExample {string} 请求参数格式:
- *  {
- *      "timestamp": 1543578242441,
- *      "accessToken": 'e2b06cd',
- *      "uid": 'user-uid'
- *  }
- *
  * @apiUse baseSuccessParams
  *
  * @apiSuccessExample {json} 正确返回值:
  *  {
- *      "data": {},
- *      "errno": 0,
- *      "errmsg": "服务器自动部署成功"
+ *      ok
  *  }
  *  @apiUse CODE_METHOD_GET_403
  *  @apiUse CODE_METHOD_POST_403
